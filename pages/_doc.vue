669d1037fb279db058e15b24c148ec5cfd0ff3fb
Peter Pike <pikepeter@gmail.com>
2021-08-31 12:51:40 +0800

    Update _doc.vue

--- pages/_doc.vue
+++ pages/_doc.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl font-bold text-gray-900 pb-8">
       {{ doc.title }}
     </h1>
-    <NuxtContent :document="doc" class="prose prose-2xl text-justify"/>
+    <NuxtContent :document="doc" class="prose prose-xl text-justify"/>
   </div>
 </template>
 
