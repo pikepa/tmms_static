89d172c8cb58e88e66402c4752ba06ed60a19762
Peter Pike <pikepeter@gmail.com>
2021-09-01 22:08:04 +0800

    format updates

--- components/DesktopSidebar.vue
+++ components/DesktopSidebar.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="bg-white border-r-2 border-gray-300">
-    <div class="flex flex-col w-56">
-      <div class="fixed h-full pt-12 overflow-y-auto justify-left">
+    <div class="flex flex-col items-center w-56">
+      <div class="fixed h-full pt-12 overflow-y-auto ">
 
        <!-- <Logo class="mt-4"/> -->
-<div >
          <img src="/images/pap.png" alt="Peter Pike" class="mt-8 rounded-full">
-</div>
 
         <ExternalLinks class="flex-col pt-12"/>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="flex flex-col items-center p-4 mx-auto">
-  <div class="text-center text-4xl">
-    <h1>Welcome to IWCPP</h1>
+  <div class="text-4xl text-center">
+    <h1>Welcome to the world of Peter Pike</h1>
   </div>
-  <div class="prose prose-xl text-justify mt-4">
+  <div class="mt-4 prose prose-xl text-justify">
       <p >
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit ut aliquam. Facilisis gravida neque convallis a. Bibendum est ultricies integer quis auctor. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Dis parturient montes nascetur ridiculus. Arcu bibendum at varius vel pharetra vel turpis nunc. Arcu cursus euismod quis viverra nibh. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed. Sit amet luctus venenatis lectus magna. Ac placerat vestibulum lectus mauris ultrices eros in. Aliquet risus feugiat in ante metus dictum. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Facilisi morbi tempus iaculis urna id. Vitae congue eu consequat ac felis donec et odio. Mi eget mauris pharetra et. Vel quam elementum pulvinar etiam non quam lacus. Blandit cursus risus at ultrices mi tempus imperdiet nulla. Tempus egestas sed sed risus. Magna ac placerat vestibulum lectus mauris ultrices eros in cursus.
     </p>
