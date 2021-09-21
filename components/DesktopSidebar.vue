d371abedc818b4e0f4f8ccd5f7966ba7c00ee94f
Peter Pike <pikepeter@gmail.com>
2021-09-21 16:35:18 +0800

    clean out some images set svg for logo

--- components/DesktopSidebar.vue
+++ components/DesktopSidebar.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="bg-white border-r-2 border-gray-300">
     <div class="flex flex-col items-center w-56">
-      <div class="fixed h-full pt-12 overflow-y-auto">
+      <div class="fixed h-full pt-8 overflow-y-auto">
         <!-- <Logo class="mt-4"/> -->
         <img
-          src="/images/tmms_logo.png"
+          src="/images/tmms_logo.svg"
           alt="TMMS Logo"
-          width="200"
-          class="flex mx-auto mt-8"
+          width="250"
+          class="flex mx-auto mt-2"
         />
 
         <!-- <ExternalLinks class="flex-col pt-12" /> -->
 
-        <DocsContents class="mt-6" />
+        <DocsContents />
       </div>
     </div>
   </div>
